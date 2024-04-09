76a07b33613da013d933d2222be8e6e358f04ed0
MC <sooogood.jc@gmail.com>
2024-04-09 21:37:31 +0800

    first commit

---
+++ PrelimCPA/Form1.Designer.vb
@@ -0,0 +1,124 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.MainButton = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.score = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Mult = New System.Windows.Forms.Label()
+        Me.Button101 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'MainButton
+        '
+        Me.MainButton.Location = New System.Drawing.Point(32, 616)
+        Me.MainButton.Name = "MainButton"
+        Me.MainButton.Size = New System.Drawing.Size(150, 46)
+        Me.MainButton.TabIndex = 0
+        Me.MainButton.Text = "Start"
+        Me.MainButton.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(32, 843)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(73, 32)
+        Me.Label1.TabIndex = 101
+        Me.Label1.Text = "Score"
+        '
+        'score
+        '
+        Me.score.AutoSize = True
+        Me.score.Location = New System.Drawing.Point(125, 844)
+        Me.score.Name = "score"
+        Me.score.Size = New System.Drawing.Size(34, 32)
+        Me.score.TabIndex = 102
+        Me.score.Text = "__"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(387, 844)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(117, 32)
+        Me.Label2.TabIndex = 103
+        Me.Label2.Text = "Multiplier"
+        '
+        'Mult
+        '
+        Me.Mult.AutoSize = True
+        Me.Mult.Location = New System.Drawing.Point(525, 845)
+        Me.Mult.Name = "Mult"
+        Me.Mult.Size = New System.Drawing.Size(34, 32)
+        Me.Mult.TabIndex = 104
+        Me.Mult.Text = "__"
+        '
+        'Button101
+        '
+        Me.Button101.Location = New System.Drawing.Point(387, 616)
+        Me.Button101.Name = "Button101"
+        Me.Button101.Size = New System.Drawing.Size(150, 46)
+        Me.Button101.TabIndex = 205
+        Me.Button101.Text = "Cheat"
+        Me.Button101.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(204, 616)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(150, 46)
+        Me.Button1.TabIndex = 206
+        Me.Button1.Text = "Reset"
+        Me.Button1.UseVisualStyleBackColor = True
+        Me.Button1.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 32.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(770, 912)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button101)
+        Me.Controls.Add(Me.Mult)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.score)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.MainButton)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MainButton As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents score As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Mult As Label
+    Friend WithEvents Button101 As Button
+    Friend WithEvents Button1 As Button
+End Class
---
+++ PrelimCPA/Form1.vb
@@ -0,0 +1,202 @@
+﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar
+Imports ICSharpCode.SharpZipLib
+Imports NPOI.OpenXmlFormats.Vml
+Imports NPOI.SS.Formula.Functions
+
+Public Class Form1
+    Dim indexx As Integer = 0
+    Dim btn(99) As Button
+    Dim RanARR(99) As Integer
+    Dim st() As Integer = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50}
+    Dim nd() As Integer = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50}
+    Dim control As Integer = 0
+
+
+    Dim countcal As Integer = 0
+    Dim primaryscore As Integer = 0
+    Dim multiplier As Integer = 1
+    Dim all As Integer = 0
+    Dim berylast As Integer = 0
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.CenterToScreen()
+        score.Text = primaryscore
+        Mult.Text = multiplier
+        Dim x As Integer = 10
+        Dim y As Integer = 10
+        For i = 0 To 99
+            btn(i) = New Button
+            btn(i).Location = New System.Drawing.Point(x, y)
+            btn(i).Size = New System.Drawing.Point(50, 50)
+            btn(i).Text = "-"
+            btn(i).Name = i
+            control = i + 1
+
+
+            Me.Controls.Add(btn(i))
+            Dim test As Integer
+            test = control Mod 10
+            If test = 0 Then
+                y = y + 55
+                x = 10
+            Else
+                x += 55
+            End If
+
+        Next
+
+
+
+    End Sub
+
+    Dim counter As Integer = 0
+    Dim one As Integer = 0
+    Dim two As Integer = 0
+    Private Sub MainButton_Click(sender As Object, e As EventArgs) Handles MainButton.Click
+
+        score.Text = 0
+        countcal = 0
+        multiplier = 1
+        Mult.Text = 1
+
+        Shuffle(st)
+        Shuffle(nd)
+        For i = 0 To 99
+            If i <= 49 Then
+                RanARR(i) = st(i)
+
+            ElseIf i >= 50 Then
+                RanARR(i) = nd(i - 50)
+            End If
+
+            btn(i).Text = RanARR(i)
+            btn(i).BackColor = Color.Black
+            AddHandler btn(i).Click, AddressOf pogi
+            btn(i).Enabled = True
+        Next
+
+        MainButton.Visible = False
+        Button1.Visible = True
+
+    End Sub
+    Private random As New Random()
+
+    Public Sub Shuffle(items As Integer())
+        Dim j As Int32
+        Dim temp As Integer
+
+        For n As Int32 = items.Length - 1 To 0 Step -1
+            j = random.Next(0, n + 1)
+
+            temp = items(n)
+            items(n) = items(j)
+            items(j) = temp
+        Next n
+    End Sub
+
+    Sub pogi(sender As Object, e As EventArgs)
+
+        Dim butones As Button = sender
+        Dim btnIndex As Integer = Convert.ToInt32(butones.Name)
+        butones.BackColor = Color.Green
+
+        If counter = 0 Then
+            one = btnIndex
+            counter += 1
+        ElseIf counter = 1 Then
+            two = btnIndex
+            counter = 0
+
+            If btn(one).Name = btn(two).Name Then
+                MessageBox.Show("oh no same button clicked")
+
+
+
+            Else
+                If btn(one).Text = btn(two).Text Then
+                    MessageBox.Show("Yehey")
+
+                    Calculatew()
+                    multiplier += 1
+                    all += 1
+                    btn(one).Enabled = False
+                    btn(two).Enabled = False
+
+                ElseIf btn(one).Text <> btn(two).Text Then
+                    MessageBox.Show("oh no")
+                    btn(one).BackColor = Color.Black
+                    btn(two).BackColor = Color.Black
+                    multiplier = 1
+                    primaryscore = countcal
+                    Calculatel()
+
+                End If
+
+            End If
+        End If
+
+
+
+    End Sub
+
+
+    Sub Calculatew()
+
+
+        If all = 0 Then
+            countcal = primaryscore + 5
+
+        ElseIf all < 5 Then
+            countcal = countcal + (5 * multiplier)
+
+        ElseIf all >= 5 Then
+
+
+            countcal = countcal + (5 * multiplier)
+            multiplier = 4
+        End If
+
+
+        score.Text = countcal
+
+
+        If multiplier = 5 Then
+            Mult.Text = multiplier
+        Else
+            Mult.Text = multiplier + 1
+        End If
+
+
+    End Sub
+
+    Sub Calculatel()
+
+        multiplier = 1
+        Mult.Text = multiplier
+        all = 0
+
+    End Sub
+
+
+
+    Dim off As Boolean = True
+    Private Sub Button101_Click(sender As Object, e As EventArgs) Handles Button101.Click
+
+        If off = True Then
+
+            off = False
+            For i = 0 To 99
+                btn(i).BackColor = Color.White
+            Next
+        ElseIf off = False Then
+            For i = 0 To 99
+                btn(i).BackColor = Color.Black
+                off = True
+            Next
+        End If
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Application.Restart()
+    End Sub
+End Class
